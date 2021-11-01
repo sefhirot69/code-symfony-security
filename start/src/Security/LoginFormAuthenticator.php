061169443e0a8ca1b98ebcdcd161758c15108140
sefhirot69 <josalillo@gmail.com>
2021-11-01 17:14:23 +0100

    feat : custom user query & credentials

--- start/src/Security/LoginFormAuthenticator.php
+++ start/src/Security/LoginFormAuthenticator.php
@@ -3,10 +3,12 @@
 namespace App\Security;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -15,6 +17,14 @@ use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
+
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function supports(Request $request): ?bool
     {
 
@@ -28,22 +38,30 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         $password = $request->request->get('password');
 
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function ($userIdentifier) {
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+                if(!$user){
+                    throw new UserNotFoundException('');
+                }
+
+                return $user;
+            }),
             new CustomCredentials(function ($credentials, User $user) {
 
-                dd($credentials, $user);
+                return $credentials === 'tada';
             }, $password)
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        dd('success');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        dd('failure');
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
