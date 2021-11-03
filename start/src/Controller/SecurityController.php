f6b9e3a0e48069b31132d1ff592d2e91b211a9c5
sefhirot69 <josalillo@gmail.com>
2021-11-03 12:49:01 +0100

    feat : authentication fail

--- start/src/Controller/SecurityController.php
+++ start/src/Controller/SecurityController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 }
